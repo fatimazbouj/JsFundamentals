e1a01bc5767ce88bfad1508ca399a99582a466be
Fatima zahra Bouj <fatima.z.bouj@gmail.com>
2024-04-30 00:19:42 +0100

    array type

---
+++ array.js
@@ -0,0 +1,16 @@
+let colors=['purple','grey','yellow'];
+
+let random =['Marrakech',12,13.5,false];
+
+console.log(random);
+
+// array methods
+
+let join = colors.join('-');
+let indexOf = random.indexOf(false);
+let concat = random.concat(colors);
+let push = colors.push('red');
+// remove last item
+colors.pop();
+
+console.log(colors);
